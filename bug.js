47b85020c73aa1dd01f41ec862fd62f3132c3b1f
BugHunter-X <askzenai@gmail.com>
2024-12-28 08:20:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,20 @@
+```javascript
+//index.js
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold underline">Hello world</h1>
+      <button onClick={() => setCount(count + 1)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Add
+      </button>
+      <p>You clicked {count} times</p>
+    </div>
+  );
+}
+
+export default App;
+```
